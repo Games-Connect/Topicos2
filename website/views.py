9db10b2bb5292975c0d58cb6f29c92db72982f57
Joao Paulo <joaopaulorpo@gmail.com>
2022-11-08 10:44:48 -0300

    Inicio do projeto

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -14,6 +15,8 @@ def home():
             flash('Note is too short!', category='error')
         else:
             new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
             flash('Note added!', category='success')
 
     return render_template("home.html", user=current_user)
