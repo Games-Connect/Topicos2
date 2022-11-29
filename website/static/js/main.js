275bcd5e649e234a3e617ea21610aa3985a1efa7
Joao Paulo <joaopaulorpo@gmail.com>
2022-11-29 16:24:29 -0300

    Finalização do projeto

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -98,4 +98,76 @@
         return false;
      });
 
+     function getPageList(totalPages, page, maxLength){
+        function range(start, end){
+            return Array.from(Array(end - start + 1), (_, i) => i + start);
+        }
+        var sideWidth = maxLength < 9 ? 1 : 2;
+        var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+        var rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+
+        if(totalPages <= maxLength){
+            return range(1, totalPages);
+        }
+
+        if(page <= maxLength - sideWidth - 1 - rightWidth){
+            return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
+        }
+
+        if(page >= totalPages - sideWidth - 1 - rightWidth){
+            return range(1, sideWidth).concat(0, range(totalPages - sideWidth - 1 - rightWidth - leftWidth, totalPages));
+        }
+
+        return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
+    }
+
+    $(function(){
+        var numberOfItems = $(".card-content .product__item").length;
+        var limitPerPage = 6;
+        var totalPages = Math.ceil(numberOfItems / limitPerPage);
+        var paginationSize = 7;
+        var currentPage;
+
+        function showPage(wichPage){
+            if(wichPage < 1 || wichPage > totalPages) return false;
+
+            currentPage = wichPage;
+
+            $(".card-content .product__item").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+
+            $(".product__pagination a").slice(1, -1).remove();
+            
+            getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+                $("<a>").toggleClass("current-page", item === currentPage)
+                .attr({href: "javascript:void(0)"}).text(item || "...").insertBefore(".next-page");
+            });
+            $(".previous-page").toggleClass("disable", currentPage === 1);
+            $(".next-page").toggleClass("disable", currentPage === totalPages);
+            return true;
+        }
+
+        $(".product__pagination").append(
+            $("<a>").addClass("previous-page").attr({href: "javascript:void(0)"}).text("Prev"),
+            $("<a>").addClass("next-page").attr({href: "javascript:void(0)"}).text("Next"),
+        );
+
+        $(".card-content").show();
+        showPage(1);
+
+        $(document).on("click", ".product__pagination a:not(.current-page)", function(){
+            if(!!parseInt($(this).text()))
+                return showPage(+$(this).text());
+        });
+
+        $(".next-page").on("click", function(){
+            debugger
+            return showPage(currentPage + 1);
+        });
+
+        $(".previous-page").on("click", function(){
+            debugger
+            return showPage(currentPage - 1);
+        });
+    });
+
 })(jQuery);
